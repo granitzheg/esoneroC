23253f0ef9e3df09848a24b355d7821cb760475f
Granit Zhegrova <44712764+granitzheg@users.noreply.github.com>
2018-11-15 20:03:31 +0100

    Update esonero.c

--- esonero.c
+++ esonero.c
@@ -11,29 +11,29 @@
         puts(" |AIUTA ROSCO A FARE UNA BUONA AZIONE, SE NON LO FARAI POTRAI RITROVARTI COME LUI, ATTENTO EHEHE :)|");
         puts("  -------------------------------------------------------------------------------------------------");
 
-        printf("\n  Inserisci il numero di giorni della predizione --> "); //chiedi all'utente di inserire il numero di giorni della predizione
+        printf("\n  Inserisci il numero di giorni della predizione --> "); //chiedo all'utente di inserire il numero di giorni della predizione
             scanf("%d", &giorni_predizione);
-        printf("  Quante azioni hai comprato? --> "); //chiedi all'utente quante azioni dispone
+        printf("  Quante azioni hai comprato? --> "); //chiedo all'utente quante azioni dispone
             scanf("%d", &azioni);
         puts("  -------------------------------------------------------------------------------------------------");
         
         puts("");
-        int array[giorni_predizione];
-        for(unsigned int i = 1; i <= giorni_predizione; i++){ //inizializzo a 1 per far partire il conteggio dei giorni da 1
-            printf("  Quanto vale l'azione al giorno %d? --> ", i); //chiedi il valore delle azioni per giorno
-                scanf("%d", &array[i]); //di conseguenza l'array partirà da 1 anzichè da 0
+        int array[giorni_predizione]; //inizializzo l'array dove inserirò il valore delle azioni per giorno
+        for(unsigned int i = 0; i < giorni_predizione; i++){
+            printf("  Quanto vale l'azione al giorno %d? --> ", i + 1); //chiedo il valore delle azioni per giorno e incremento i giorni di 1 per convenienza (partirà da giorno 1 anzichè 0)
+                scanf("%d", &array[i]);
         }
         puts("");
 
-        for(unsigned int i = 1; i <= giorni_predizione; i++){
-            for(unsigned int j = i + 1; j <= giorni_predizione; j++){
+        for(unsigned int i = 0; i < giorni_predizione; i++){
+            for(unsigned int j = i + 1; j < giorni_predizione; j++){
                 
                 cerca_intervallo = array[j] - array[i]; //cerco l'intervallo maggiore sottraendo gli elementi tra di loro
 
-                if(cerca_intervallo > intervallo){ //trovo l'intervallo maggiore e lo salvo in una variabile(l'intervallo maggiore è quello migliore)
+                if(cerca_intervallo > intervallo){ //trovo l'intervallo maggiore e lo salvo in una variabile (l'intervallo maggiore è quello migliore)
                     intervallo = cerca_intervallo;
-                        primo_giorno = i + 1; //una volta trovato l'intervallo maggiore memorizzo in due variabili i rispettivi indici dell'array //incremento di 1 perchè 
-                        ultimo_giorno = j; //che equivalgono all'inizio e alla fine dell'intervallo
+                        primo_giorno = i + 2; //incremento di 2 perchè un posto serve per scalare nell'indice (per convenienza) e l'altro perchè si compra la mattina del giorno dopo
+                        ultimo_giorno = j + 1; //una volta trovato l'intervallo maggiore memorizzo in due variabili i rispettivi indici dell'array che equivalgono all'inizio e alla fine dell'intervallo
                             primo_giorno_valore = array[i]; //mentre qui memorizzo i valori dei rispettivi indici
                             ultimo_giorno_valore = array[j];
                 }
